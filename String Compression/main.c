1ace22740d16a139c7e0884a39638a1965ced483
Scranoid <jhabhinav50@gmail.com>
2024-10-22 02:32:49 +0530

    Update main.c

--- String Compression/main.c
+++ String Compression/main.c
@@ -9,6 +9,8 @@ This is rather a tricky set of code so I'll explain it by breaking it into simpl
 At first we define a constant named MAXINP and giving it value of 100 which is going to be used to define the char array's memory named as str.
 Now , the main calculative part is done in the body of the FOR loop before that I initialized two very important things for the functioning of this program,
 I declared the first letter as current and took count as the variable for storing the number of variables as 1;
+After which I declared a FOR loop which will be using an IF ELSE statement;
+Now , about the variables I used count to append the value of repeating characters into count and so forth , the ELSE statement prints the values;
 */
 int main() {
     char str[MAXINP];
