d23dd900223f341f2ff94dc5d9471aa3b9027717
Scranoid <jhabhinav50@gmail.com>
2024-10-21 22:06:25 +0530

    Add files via upload

---
+++ Climbing Stairs/main.cpp
@@ -0,0 +1,34 @@
+#include<stdio.h>
+/*
+This program takes the input from the user of the number of stairs he or she might want to climb and then calculates the number of distinct ways the person can climb
+that stair climbing only 1 or 2 step at a time.
+We have used two different functions to execute this whole program one being climbStairs for the main part of the calculations and the other one being main to execute and
+call the climbStairs function.
+There are four different functions defined and used int this entire program each one of them being:
+1. n being the integer which will help in getting the values out to call the climbStairs function.
+2. prev1 and prev2 being the integers used in assigning and calculating the values of number of discrete steps in the calculation.
+3. current being the integer to be assigned .
+4. x being the integer to store the number of steps the user might want to climb up to and is helping in calling the climbStairs function.
+ */
+int climbStairs(int n) {
+    if ( n <= 1 ) {
+        return 1;
+    }
+
+    int prev1 = 1, prev2 = 1;
+    for (int i = 2; i <= n; ++i) {
+        int current = prev1 + prev2;
+        prev2 = prev1;
+        prev1 = current;
+    }
+
+    return prev1;
+}
+
+int main() {
+    printf("How many stairs do you want to climb?\n");
+    int x;
+    scanf("%d" , &x);
+    printf("Number of ways to climb %d stairs: %d\n", x, climbStairs(x));
+    return 0;
+}
