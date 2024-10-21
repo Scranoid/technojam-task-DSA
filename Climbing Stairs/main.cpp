68fcc23bdf7f8fbbf2049543ee39e700970579ad
Scranoid <jhabhinav50@gmail.com>
2024-10-22 01:20:56 +0530

    Update main.cpp

--- Climbing Stairs/main.cpp
+++ Climbing Stairs/main.cpp
@@ -1,5 +1,8 @@
 #include<stdio.h>
 /*
+As said earlier in Pascal's Triangle;
+I'm Abhinav Jha and all of this documentation is done by me to make my program Readable and more understable for the fellow programmer , Along with this I Follow 
+good programming practices which incorporate proper WHITESPACING and INDENTAION. So , Don't even consider this as an AI generated SLOP. :)
 This program takes the input from the user of the number of stairs he or she might want to climb and then calculates the number of distinct ways the person can climb
 that stair climbing only 1 or 2 step at a time.
 We have used two different functions to execute this whole program one being climbStairs for the main part of the calculations and the other one being main to execute and
